f18b238c8519f074d10978905b7692641a620d0b
Irem Deniz Gunduz <zeynepgunduz94@hotmail.com>
2021-07-01 22:21:30 +0200

    Subscription form created without submit button functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SubscriptionApp';
+
+  formGroup: FormGroup;
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      Email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+      ]),
+      Phone: new FormControl('', [
+        Validators.required,
+        Validators.minLength(9)
+      ]),
+      Name: new FormControl('', [
+        Validators.required,
+      ])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formGroup);
+  }
 }
